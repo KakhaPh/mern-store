d160fb1b106684383f2584b3369e8ee38f474357
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-04 23:38:39 +0400

    server.js put method

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { ConnectDB } from './config/db.js';
 import Product from './model/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.get("/api/products", async (req, res) => {
         res.status(200).json({ success: true, data: products });
     } catch (error) {
         console.log("error in fetching product", error.message)
-        res.status(500).json({success: false, message: "Server Error!"});
+        res.status(500).json({ success: false, message: "Server Error!" });
     }
 });
 
@@ -45,6 +46,22 @@ app.delete("/api/products/:id", async (req, res) => {
     } catch (error) {
         console.log("error in deleting product", error.message)
         res.status(404).json({ success: false, message: "Product not found!" })
+    }
+})
+
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid product Id!" })
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.log("error in updating product", error.message)
+        res.status(500).json({ success: false, message: "Server Error!" });
     }
 })
 
