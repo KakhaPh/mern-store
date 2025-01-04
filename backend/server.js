882a72519ea29fe1ad2c3bc171e6ba5851b1a3ee
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-04 21:08:04 +0400

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { ConnectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send("Server is ready!")
+})
+
+console.log(process.env.MONGO_URI)
+
 app.listen(5000, () => {
+    ConnectDB();
     console.log("Server started at http://localhost::5000")
 })
