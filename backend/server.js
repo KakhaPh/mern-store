eea40a0fdd51ea9283455fdbb716d12a1bdaa013
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-04 22:42:46 +0400

    Back app.add & app.delete

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,29 @@ app.post("/api/products", async (req, res) => {
     const product = req.body
 
     if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please fill all fields!" })
+        res.status(400).json({ success: false, message: "Please fill all fields!" })
     }
     const newProduct = new Product(product);
 
     try {
         await newProduct.save();
-        return res.status(201).json({ success: true, data: newProduct })
+        res.status(201).json({ success: true, data: newProduct })
     } catch (error) {
         console.error("Error in create product!", error.message);
-        return res.status(500).json({ success: true, message: "Server Error!" })
+        res.status(500).json({ success: true, message: "Server Error!" })
     }
 });
 
-console.log(process.env.MONGO_URI)
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted!" })
+    } catch (error) {
+        res.status(404).json({success: false, message: "Product not found!"})
+    }
+})
 
 app.listen(5000, () => {
     ConnectDB();
