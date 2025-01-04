6aa623a23e2125cdfd1bfb8a16ce9ad02c5a5bf7
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-04 21:48:15 +0400

    server.js + create model collection for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { ConnectDB } from './config/db.js';
+import Product from './model/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Server is ready!")
-})
+app.use(express.json())
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please fill all fields!" })
+    }
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        return res.status(201).json({ success: true, data: newProduct })
+    } catch (error) {
+        console.error("Error in create product!", error.message);
+        return res.status(500).json({ success: true, message: "Server Error!" })
+    }
+});
 
 console.log(process.env.MONGO_URI)
 
 app.listen(5000, () => {
     ConnectDB();
-    console.log("Server started at http://localhost::5000")
+    console.log("Server started at http://localhost:5000")
 })
