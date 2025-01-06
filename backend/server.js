7f21623f26cdb772c6dd10ed8617b0252b7444b2
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-06 10:40:30 +0400

    Code correction

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import productRoutes from './routes/product.route.js'
 dotenv.config();
 
 const PORT = process.env.PORT
+
 const app = express();
+
 app.use(express.json())
 
 app.use("/api/products", productRoutes)
